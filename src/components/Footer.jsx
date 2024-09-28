59aa8636130ca9a9501ae7c81dd908804b18bee6
YuriSP <yuri.dinodude08@gmail.com>
2024-09-28 18:40:56 -0300

    Arrumei o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer =()=> {
                 <h5> 
                     @2024 - Todos Direitos Reservados - Musicality
                 </h5>
-                    <a href="https://github.com/jota0802/ProjetoFrontEnd-CP1/blob/main/README.md"><i class="fi fi-brands-github"></i></a>
+                    <a href="https://github.com/yurisilpess/music-store"><i class="fi fi-brands-github"></i></a>
                     <a href=""><i class="fi fi-brands-whatsapp"></i></a>
                     <a href=""><i class="fi fi-brands-linkedin"></i></a>
                     <a href=""><i class="fi fi-brands-instagram"></i></a>
